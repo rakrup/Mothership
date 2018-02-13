11a978d786ea4fba4dcfe2e085661f61bac3f046
Rahul <rakrup@gmail.com>
2018-02-13 11:37:31 +0530

    Checking-in the packer.sh script Moving packer config in a config dir Updating the .gitignore

--- packer/scripts/packer.sh
+++ packer/scripts/packer.sh
@@ -1,3 +1,42 @@
+#/bin/bash
+COCKPIT='/home/rahul/Mothership'
+PACKERCONFIG=$1
+UUID=`uuidgen`
+LOGFILE=$COCKPIT/log/$UUID.log
+OUTPUTDIR=$COCKPIT/output/$UUID
+source $COCKPIT/creds/aws.creds
+ACCOUNT_FILE=$COCKPIT/creds/account.json
+source $COCKPIT/creds/DO.creds
+TIME=`date`
+ANSIBLE_LOCATION=$COCKPIT/packer/ansible/apache/apache.yml
+ANSIBLE_EXTRA_VARS='["--extra-vars","ansible_sudo_pass=rahul"]'
 
-packer build  -var 'aws_access_key=AKIAJK33DVH3SS6YNJXQ' -var 'aws_secret_key=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' packerex.json > packer_logs.txt
-AMI=`grep ami packer_logs.txt  | grep west| grep -v ec2  | awk -F':' '{print $2}'`
+touch "$LOGFILE"
+echo "Starting a build with Build-ID : $UUID"
+echo "Tail the logs at $LOGFILE for more details.."
+packer build  -var "aws_access_key=$AWS_ACCESS_KEY" -var "aws_secret_key=$AWS_SECRET_KEY" -var "google_account_file=$ACCOUNT_FILE" \
+       -var "digitalocean_api_key=$DO_API_KEY"	-var "ansible_location=$ANSIBLE_LOCATION" \
+       -var "ansible_extra_params=$ANSIBLE_EXTRA_VARS=" $PACKERCONFIG > $LOGFILE
+AMI=`grep ami $LOGFILE  | grep west| grep -v ec2  | awk -F':' '{print $2}'`
+DOI=`grep digitalocean $LOGFILE| grep snapshot |grep created | awk -F "'" '{print $2}'`
+GCP=`grep googlecompute $LOGFILE| grep 'disk image was created' | awk -F ":" '{print $3}'`
+
+echo "--------------------------------------"
+echo "|             Build Result           | "
+echo "--------------------------------------"
+echo "|UUID         | $UUID "
+echo "|AMI-id       | $AMI"
+echo "|Digital Ocean| $DOI"
+echo "|Google CLoud | $GCP"
+echo "--------------------------------------"
+echo "Copy of result also present in the $OUTPUTDIR"
+
+mkdir -p $OUTPUTDIR
+echo "BUILD START TIME : $TIME" >> $OUTPUTDIR/build.result
+echo "UUID : $UUID" >> $OUTPUTDIR/build.result
+echo "AMI : $AMI" >> $OUTPUTDIR/build.result
+echo "DigitalOcean : $DOI" >> $OUTPUTDIR/build.result
+echo "GoogleCloud : $GCP" >> $OUTPUTDIR/build.result
+
+cp package.list $OUTPUTDIR/package.list
+rm package.list
