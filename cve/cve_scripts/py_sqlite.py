823adcbe5ae1628d75f35077ca916f87baa10dc7
Manoj Rana <manojrana.k1@gmail.com>
2018-02-13 09:56:32 +0530

    added sqlite py package

---
+++ cve/cve_scripts/py_sqlite.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+def db_create(mydb,c_query):
+    connection = sqlite3.connect(mydb)
+    with connection:
+        cursor = connection.cursor()
+        #c_query = "create table if not exists build_details(CVE string, package string,ptime string)"
+        cursor.execute(c_query)
+        connection.commit()
+
+    if connection:
+       connection.close()
+
+def db_insert(mydb,i_query):
+    connection = sqlite3.connect(mydb)
+    with connection:
+        print i_query
+        cursor.execute(i_query)
+        connection.commit()
+
+    if connection:
+       connection.close()
+
